1ec9f682a2e888371592366744f79d1ca57af796
Stephan Heßelmann <shesselm@redhat.com>
2026-05-07 10:58:29 +0200

    fix pre-commit

--- nix/pre-commit.nix
+++ nix/pre-commit.nix
@@ -19,22 +19,8 @@
     };
 
     # Builtin pre-commit hooks
-    check-merge-conflict = {
-      enable = true;
-      name = "check for merge conflicts";
-      description = "checks for files that contain merge conflict strings.";
-      entry = "${pkgs.python3Packages.pre-commit-hooks}/bin/check-merge-conflict";
-      language = "python";
-      types = ["text"];
-    };
-    detect-private-key = {
-      enable = true;
-      name = "detect private key";
-      description = "detects the presence of private keys.";
-      entry = "${pkgs.python3Packages.pre-commit-hooks}/bin/detect-private-key";
-      language = "python";
-      types = ["text"];
-    };
+    check-merge-conflict.enable = true;
+    detect-private-key.enable =  true;
   };
 
   package = pkgs.prek;
